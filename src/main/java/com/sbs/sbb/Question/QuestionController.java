ce7a1a42b9f163dc090f8cff5fe9e566d0e580ed
unknown <gsw05018@naver.com>
2023-10-13 11:01:57 +0900

    QuestionController 수정, questionList.html ì리스트 출력

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -1,16 +1,33 @@
 package com.sbs.sbb.Question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor // loombok 라이브러리로 final에 대해 자동으로 생성자 생성
 @Controller
 public class QuestionController {
 
+    // questionRepository 연결
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String list(){
-        return "Question_list";
-    // html을 출력하기 위해서는 Reposbody를 지우고 getmapping만 사용해 링크를 연결해준다
+    public String list(Model model){
+
+        // questionRepositroy에서 모든 질문 목록을 가져옴
+        List<Question> questionList = this.questionRepository.findAll();
+
+        // model에서 questionlist를 추가하여 view에서 사용할 수 있게 함
+        model.addAttribute("questionList", questionList);
+
+        return "question_list";
+    // html을 출력하기 위해서는 Reposbody를 지우고 getmapping만 사용해 링크를 연결해줌
+    // question_list라는 이름의 템플릿을 렌더링하여 반환
+
     }
 
 }
