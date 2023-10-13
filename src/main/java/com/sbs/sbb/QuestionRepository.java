024cc604f4dfa279ca2da8c1302286e160fe1d0f
unknown <gsw05018@naver.com>
2023-10-13 10:25:42 +0900

    application.yml 코드 해석 및 쿼리문 출력 추가

--- src/main/java/com/sbs/sbb/QuestionRepository.java
+++ src/main/java/com/sbs/sbb/QuestionRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     // JapRepository<엔티티 타입, pk 속성>
+
+    Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
+
 }
