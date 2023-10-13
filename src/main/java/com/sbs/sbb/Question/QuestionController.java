27bbef324f131de81ac8852e42dc49cc0a8310f0
unknown <gsw05018@naver.com>
2023-10-13 11:11:05 +0900

    root  작성

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -30,4 +30,12 @@ public class QuestionController {
 
     }
 
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+    // 홈으로 이동시 리다이렉션하여 질문목록으로 이동시킴
+
+
+
 }
