d9cfc46ca723abf629823e16b6f791b7032aee08
unknown <gsw05018@naver.com>
2023-10-16 10:27:15 +0900

    테스트 데이터 생성

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -2,6 +2,7 @@ package com.sbs.sbb;
 
 import com.sbs.sbb.question.Question;
 import com.sbs.sbb.question.QuestionRepository;
+import com.sbs.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,20 +13,17 @@ import java.time.LocalDateTime;
 class SbbApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		for(int i=0; i<300; i++){
+			String subject = String.format("테스트 데이터[%s]", i);
+			String content = "내용";
+			this.questionService.create(subject, content);
+		}
+
+
 	}
 }
